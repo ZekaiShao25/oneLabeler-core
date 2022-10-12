385110bd3a33b3f8055001c36b607d519ddf6599
Yu Zhang <yuzhang94@outlook.com>
2022-10-12 10:46:38 +0800

    feat: export Annotation type

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import * as dataTypeText from './components/data-types/text'
 import * as labelTaskRelation from './components/label-tasks/relation'
 import * as labelTaskSpan from './components/label-tasks/span'
 import { useStore as useAnnotationStore } from './components/label-tasks/store'
+import type { Annotation } from './components/label-tasks/types'
 
 export {
   dataTypeImage,
@@ -13,3 +14,5 @@ export {
   labelTaskSpan,
   useAnnotationStore,
 }
+
+export type { Annotation }
